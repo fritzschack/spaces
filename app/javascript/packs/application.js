332ead2d27d5bd2d76922527fd9b0445f9ed7e6f
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-02-28 12:47:42 +1100

    Maps Done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,15 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { autoScrollHome } from '../components/autoScrollHome';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
+
+initAutocomplete();
+initMapbox();
 initUpdateNavbarOnScroll();
 autoScrollHome();
 
