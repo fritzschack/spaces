a173bde6fb841c42e83caff36e3b7fb7a7a0203e
Fritz Schack <fritz.schack@gmail.com>
2019-02-28 14:42:13 +1100

    Finished the photo gallery on the show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,14 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+import Siema from 'siema';
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { autoScrollHome } from '../components/autoScrollHome';
 
 initUpdateNavbarOnScroll();
 autoScrollHome();
+
+const mySiema = new Siema();
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
 
